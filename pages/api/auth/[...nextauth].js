56fccfb3bd67495758c26f7d8806e036c36294ee
Djafar <djafar@example.com>
2022-07-18 15:43:43 +0500

    asd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,70 +1,74 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+// import jwt from "jsonwebtoken"
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
+      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Assistant",
       // The credentials is used to generate a suitable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
+        // Add logic here to look up the user from the credentials supplied
 
+        let user ;
 
         const res = await fetch(`${ process.env.NEXTAUTH_URL }/api/auth/authUser`, {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        })
-          .then((res) => res.json())
-          .then(
-            (result) => {
-              console.log('sad');
-              console.log(result.ok );
-              if (result.ok) {
-                return result.user;
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: { "Content-Type": "application/json" },
+          })
+            .then((res) => res.json())
+            .then(
+              (result) => {
+                if (result.ok) {
+                  console.log(result);
+
+                  user = result.user
+                  
+                  return user;
+                }
+                // Return null if user data could not be retrieved
+                return null;
+              },
+              (err) => {
+                console.log(err);
               }
-              // Return null if user data could not be retrieved
-              return null;
-            },
-            (err) => {
-              console.log(err);
-            }
-          );
+            );
 
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
 
-        // If no error and we have user data, return it
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+
       },
     }),
   ],
-  // secret: "test",
-  jwt: {
-    secret: "test",
-    encryption: true,
-  },
+
   secret: process.env.NEXTAUTH_SECRET,
+
   callbacks: {
-    async session({ session, token }) {
-      session.address = 'asd'
-      session.user.name = 'asd'
+    jwt: async ({ token, user }) => {
+      console.log('==dddsss==');
+      console.log(token);
+      user && (token.user = user)
+      return token
+    },
+    session: async ({ session, token }) => {
+      session.user = token.user // Setting token in session
       return session
     },
-  },
-});
+  }
+})
