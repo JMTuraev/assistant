290c81c9dfb56c06faf9d9e2d356b3f8a9946a56
jafar <jafaralituraev@gmail.com>
2022-07-27 18:28:15 +0500

    user

--- components/new/RightBlock/RightBlock.js
+++ components/new/RightBlock/RightBlock.js
@@ -8,20 +8,7 @@ export default function RightBlock({  whoToFollow,  show, setShowCreateCompany, 
     <aside className="hidden xl:block xl:col-span-2">
       <div className="sticky top-4 ">
       <div className={  show === 'Организация' ? 'visible' : 'hidden' }>
-      <section>
-        <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 mb-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={()=>setShowCreateCompany(!showCreateCompany)}
-          >
-            <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-            Добавить новая организация
-          </button>
-        </section>
-
-        <section aria-labelledby="who-to-follow-heading">
-          <CompaniesList show={show}  whoToFollow={whoToFollow} companies={companies} onActive={onActive} />
-        </section>
+          {/* code     */}
       </div>
 
       <div className={  show === 'Моя команда' ? 'visible' : 'hidden' }>
